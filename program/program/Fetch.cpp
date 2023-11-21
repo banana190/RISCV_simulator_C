55ca0a396f5cce17c52850f083269fa9f4429fb1
banana190 <playgametu@gmail.com>
2023-11-20 19:37:54 -0500

    fix \:

--- program/program/Fetch.cpp
+++ program/program/Fetch.cpp
@@ -7,20 +7,22 @@
 
 using namespace std;
 
-vector<string> fetch1() {
+vector<string> fetch1()
+{
     cout << "\n------------------------------------------------------------------------------------------------\n";
     cout << "How many instructions do you want to enter?\n";
     cout << "number:";
     int num;
     string ins;
     cin >> num;
-    //don't know why, first getline won't get any string
+    // don't know why, first getline won't get any string
     getline(cin, ins);
 
     vector<string> s;
 
-    for (int i = 1; i <= num; i++) {
-        cout << "\nInstruction" << i << " \:";
+    for (int i = 1; i <= num; i++)
+    {
+        cout << "\nInstruction" << i << " :";
         getline(cin, ins);
         s.push_back(ins);
     }
